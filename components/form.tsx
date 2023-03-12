9f56052c5bd4a07f32dd0df1b0790460a80b9152
soomro30 <soomro30@gmail.com>
2023-03-12 15:38:19 +0400

    Added react datepicker and fixed error

--- components/form.tsx
+++ components/form.tsx
@@ -114,7 +114,7 @@ const Form = () => {
           
           className='block w-full px-4 py-3 mt-4 rounded-md bg-slate-400 dark:placeholder-slate-300 dark:bg-slate-800 px-2 py-5 outline-none placeholder-slate-700'
           /> */}
-          <DatePicker className='w-full mt-4 rounded-md bg-slate-400 dark:placeholder-slate-300 dark:bg-slate-800 px-2 py-5 outline-none placeholder-slate-700' selected={startDate}   onChange={(date) => setStartDate(date)} placeholderText='Please select a date'  />
+          <DatePicker className='w-full mt-4 rounded-md bg-slate-400 dark:placeholder-slate-300 dark:bg-slate-800 px-2 py-5 outline-none placeholder-slate-700' selected={startDate} onChange={(date: Date) => setStartDate(date!)} />
 
           <select
              className='block w-full px-4 py-3 mt-4 rounded-md bg-slate-400 dark:placeholder-slate-300 dark:bg-slate-800 px-2 py-5 outline-none placeholder-slate-700'
